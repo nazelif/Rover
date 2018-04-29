1ae3be5854512cfede0db9cbd04fec71dcc91d32
Elif Naz Samanci <elifnazsamanci@gmail.com>
2018-04-29 13:17:49 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,20 @@
+#This file to keep the variables
+#Probably this is what we will use to radio transmit
+traveling_factor = 10
+ascending_slope_factor = 200
+descending_slope_factor = 10
+r = 10 #radius of wheel
+size_of_cell = 1 #for now
+start_cell = (0,0) #row 1 col 0
+goal_cell = (10,10) #row 1 col 3
+initial_angle = 90 #Where we are facing...   Currently +y axis direction
+max_slope = 5 #max slope we want to bear
+max_rotation_angle = 180
+nx = 36 #360
+ny = 36 #360
+sizex = 3 #30
+sizey = 1 #10
+m = 15 #30
+n = 17 #34
+x_size = 2
+y_size = 2
